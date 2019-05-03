d3a6377974e154046929b6b24f3983200e982310
l <l@localhost.localdomain>
2019-05-03 14:34:15 +0800

    optimized and rewritten vigenere.py

--- vigenere.py
+++ vigenere.py
@@ -7,50 +7,38 @@ def main():
     """
 
     import sys
-    from string import ascii_lowercase as lowercase, ascii_uppercase as uppercase
-    import argparse
-    parser = argparse.ArgumentParser()
-    parser.add_argument("secret_keyword", help="Enter string of text for the secret_keyword")
-    args = parser.parse_args()
+    argv = sys.argv
 
-    secret_keyword = str(args.secret_keyword)
+    if len(argv) != 2 or argv[1].isalpha() != True:
+        sys.exit("Usage: python vigenere.py cipher")
+    secret_keyword = str(argv[1])
+
     secret_key = list()
-    ascii = lowercase + uppercase
     for key_char in secret_keyword:
-        index_number = ascii.find(key_char) if key_char in ascii else sys.exit()
-        secret_key.append(index_number)
+        secret_key.append(ord(key_char.lower()) - 97)
 
     plain_text = input('plain_text: ')
     plain_text_mapping = list()
-    for each_plain_char in plain_text:
-        index_number = ascii.find(each_plain_char) if each_plain_char in ascii else each_plain_char
-        plain_text_mapping.append(index_number)
+    for char in plain_text:
+        plain_text_mapping.append(ord(char))
     key_index = 0
-    all_cipher = list()
-    max_length = len(secret_key) - 1
-    for ascii_position in plain_text_mapping:
-        if key_index > max_length:
-            key_index = 0
-        if type(ascii_position) == int:
-            if ascii_position >= 26:
-                cipher_number = ascii_position + secret_key[key_index] 
-                if cipher_number >= 52:
-                    cipher_number = (key_index % 52) + 26
-                key_index += 1
+    all_cipher = str()
+    max_length = len(secret_key)
+    for ascii_number in plain_text_mapping:
+        if key_index >= max_length:
+            key_index = 0 
+        if chr(ascii_number).isalpha():
+            if chr(ascii_number).islower():
+                cipher_char = chr(((((ascii_number - 97) + secret_key[key_index]) % 26) + 97))
             else:
-                cipher_number = ascii_position + secret_key[key_index]
-                if cipher_number >= 26:
-                    cipher_number %= 26 
-                key_index += 1 
+                cipher_char = chr(((((ascii_number - 65) + secret_key[key_index]) % 26) + 65))
+            key_index += 1
         else:
-            cipher_number = ascii_position
-        all_cipher.append(cipher_number) 
-         
-    final_print = str()
-    for each_cipher_char in all_cipher:
-        printable = ascii[each_cipher_char] if type(each_cipher_char) == int else each_cipher_char
-        final_print += printable 
-    print (f"cipher_text: {final_print}")
+            cipher_char = (chr(ascii_number))
+
+        all_cipher += cipher_char
+        
+    print (f"ciphertext: {all_cipher}\n")
 
 
 if __name__=='__main__':
